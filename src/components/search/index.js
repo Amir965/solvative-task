e63cc8e80417f105f5ce0985ff7b214ee311d7d9
Amir965 <sohailaamir965@gmail.com>
2023-01-15 14:30:42 +0530

    ctrkey update

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,9 +1,20 @@
 import React from "react";
 import "./Search.css";
 const Search = ({ setSearch, search }) => {
+  window.addEventListener("keydown", function (e) {
+    if (e.keyCode === 114 || (e.ctrlKey && e.keyCode === 191)) {
+      if (document.getElementById("search") !== document.activeElement) {
+        e.preventDefault();
+        document.getElementById("search").focus();
+      } else {
+        return true;
+      }
+    }
+  });
   return (
     <div className="search">
       <input
+        id="search"
         type="search"
         placeholder="search places"
         value={search}
